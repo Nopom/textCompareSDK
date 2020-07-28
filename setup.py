8711e9fd7461228b895bd46ead38c08e3d60e747
Nopom <39113768+Nopom@users.noreply.github.com>
2020-07-28 11:12:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
 	name = "demo",
 	version = "0.1",
-	url = 'https://github.com/snowroll/python-sdk.git',
+	url = 'https://github.com/Nopom/textCompareSDK.git',
 	long_description = open('README.md').read(),
 	packages = find_packages(),
 )
